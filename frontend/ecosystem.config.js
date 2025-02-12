57c8e49976fe3f8415a6b9c2e6e983ce3369d1a8
kaurovaae <kaurova.a.e@gmail.com>
2025-02-12 17:31:04 +0300

    feat: pm2 deploy settings

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config({ path: '../.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPOSITORY,
+} = process.env;
+
+module.exports = {
+  // Настройка деплоя фронтенда
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
+    },
+  },
+};
